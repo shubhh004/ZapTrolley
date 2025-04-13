ecfde4ddaa27eae86a37e1a55c8fb34fb90c8e2b
devansh2991 <devansh.kushwah90@gmail.com>
2025-04-13 10:56:01 +0530

    Initial commit

---
+++ src/main/java/io/github/devansh2991/zaptrolley/ZapTrolleyApplication.java
@@ -0,0 +1,13 @@
+package io.github.devansh2991.zaptrolley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZapTrolleyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZapTrolleyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/devansh2991/zaptrolley/ZapTrolleyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.devansh2991.zaptrolley;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZapTrolleyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
